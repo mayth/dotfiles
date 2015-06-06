dc114578631a0624ef470335efcbb3a8496f62e3
mayth <chimeaquas@hotmail.com>
2015-06-06 23:28:26 +0900

    add general setup script

--- setup.sh
+++ setup.sh
@@ -1,14 +1,28 @@
 #!/bin/sh
 
-set -ue
-
 dotfiles_dir=$(cd "$(dirname "$0")"; pwd)
-#setup/rbenv.sh
-#setup/pyenv.sh
-#setup/ndenv.sh
 
-ln -s "${dotfiles_dir}/_zshrc" "${HOME}/.zshrc"
-ln -s "${dotfiles_dir}/zshfiles" "${HOME}/.zsh"
-ln -s "${dotfiles_dir}/_vimrc" "${HOME}/.vimrc"
-ln -s "${dotfiles_dir}/vimfiles" "${HOME}/.vim"
+function lnshome() {
+    # echo "${dotfiles_dir}/$1 -> ${HOME}/$2"
+    if [ ! -f "${HOME}/$2" ] && [ ! -d "${HOME}/$2" ]; then
+        ln -s "${dotfiles_dir}/$1" "${HOME}/$2"
+    else
+        echo "${HOME}/$2 already exists"
+    fi
+}
 
+### init submodule
+(cd "${dotfiles_dir}"; git submodule update --init)
+
+### create symbolic link to $HOME
+lnshome _zshrc .zshrc
+lnshome zshfiles .zsh
+lnshome _vimrc .vimrc
+lnshome vimfiles .vim
+lnshome _tmux.conf .tmux.conf
+lnshome _screenrc .screenrc
+
+### run *env setup script
+setup/rbenv.sh
+setup/pyenv.sh
+setup/ndenv.sh
