184740b56ca967364812b1e689ec7333e32af56f
mayth <chimeaquas@hotmail.com>
2015-06-06 23:28:06 +0900

    skip *env setup when they are already installed

--- setup/ndenv.sh
+++ setup/ndenv.sh
@@ -8,6 +8,11 @@ node_build_repo='https://github.com/riywo/node-build.git'
 ndenv_root=${NDENV_ROOT:-${HOME}/.ndenv}
 ndenv_plugin_dir="${ndenv_root}/plugins"
 
+if [ -d "$ndenv_root" ] || type ndenv > /dev/null 2>&1; then
+    echo "ndenv is already installed" 1>&2
+    exit 1
+fi
+
 # install
 git clone "$ndenv_repo" "$ndenv_root"
 mkdir -p "$ndenv_plugin_dir"
--- setup/pyenv.sh
+++ setup/pyenv.sh
@@ -12,6 +12,11 @@ pyenv_doctor_repo='https://github.com/yyuu/pyenv-doctor.git'
 pyenv_root=${PYENV_ROOT:-${HOME}/.pyenv}
 pyenv_plugin_dir="${pyenv_root}/plugins"
 
+if [ -d "$pyenv_root" ] || type pyenv > /dev/null 2>&1; then
+    echo "pyenv is already installed" >&2
+    exit 1
+fi
+
 git clone "$pyenv_repo" "$pyenv_root"
 mkdir -p "$pyenv_plugin_dir"
 git clone "$pyenv_virtualenv_repo" "${pyenv_plugin_dir}/pyenv-virtualenv"
